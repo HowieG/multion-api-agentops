bc13bf08583978702d26fa6d85a0246f3b909e1f
Howard Gil <howardbgil@gmail.com>
2024-03-11 12:33:13 -0700

    Working example picking up request body

---
+++ examples/test_request_patching.py
@@ -0,0 +1,3 @@
+import multion
+multion.login(use_api=True, multion_api_key="0a4f3bc7361f4ec1aac78fae687808fb")
+multion.set_remote(True)
--- sdks/multion-py/multion/agentops_client.py
+++ sdks/multion-py/multion/agentops_client.py
@@ -1,5 +1,9 @@
 from agentops import Client, ActionEvent, ErrorEvent
 from typing import List
+from opentelemetry.trace.status import Status, StatusCode
+from opentelemetry import trace
+from functools import wraps
+import requests
 
 
 class AgentOpsClient:
@@ -21,5 +25,29 @@ class AgentOpsClient:
         if self.client:
             self.client.end_session(end_state=end_state, video=video_url)
 
+
+def trace_request(func):
+    """Decorator to trace HTTP requests made with the `requests` library."""
+
+    @wraps(func)
+    def wrapper_trace_request(*args, **kwargs):
+        tracer = trace.get_tracer(__name__)
+        with tracer.start_as_current_span("agentops-patched-request-body", kind=trace.SpanKind.CLIENT) as span:
+            if 'json' in kwargs:
+                span.set_attribute("http.request.body", str(kwargs['json']))
+            elif 'data' in kwargs:
+                span.set_attribute("http.request.body", str(kwargs['data']))
+
+            try:
+                response = func(*args, **kwargs)
+                return response
+            except Exception as e:
+                span.set_status(Status(StatusCode.ERROR, description=str(e)))
+                raise
+
+    return wrapper_trace_request
+
+
+requests.post = trace_request(requests.post)
 
 __all__ = ['AgentOpsClient', 'ActionEvent']
